c4b5c95bd831f75e658ab23c419ab88928404a0e
Minh12s <minhtnth2209037@fpt.edu.vn>
2023-05-11 13:45:10 +0700

    1

---
+++ src/btapSlot10/Phone.java
@@ -0,0 +1,15 @@
+package btapSlot10;
+
+public abstract class Phone {
+        public abstract void insertPhone(String name, String phone);
+
+        public abstract void removePhone(String name);
+
+        public abstract void updatePhone(String name,String newphone);
+
+        public abstract void searchphone (String name);
+
+        public abstract void sort();
+
+
+}
---
+++ src/btapSlot10/PhoneBook.java
@@ -0,0 +1,38 @@
+package btapSlot10;
+
+
+import java.util.ArrayList;
+
+
+public class PhoneBook extends Phone{
+    ArrayList<String> PhoneList = new ArrayList<>();
+
+
+    @Override
+    public void insertPhone(String name, String phone) {
+
+
+
+
+    }
+
+    @Override
+    public void removePhone(String name) {
+
+    }
+
+    @Override
+    public void updatePhone(String name, String newphone) {
+
+    }
+
+    @Override
+    public void searchphone(String name) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
