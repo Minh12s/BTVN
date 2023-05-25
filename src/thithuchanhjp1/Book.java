ff10e2cdf03eaa120fac2105bbc699eacc3ccf5a
Minh12s <minhtnth2209037@fpt.edu.vn>
2023-05-25 15:30:38 +0700

    1

---
+++ src/thithuchanhjp1/Book.java
@@ -0,0 +1,41 @@
+package thithuchanhjp1;
+
+public class Book {
+    String title;
+     Author name;
+    double price;
+
+    public Book(String title,Author name, double price) {
+        this.title = title;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getName() {
+        return name;
+    }
+
+    public void setName(Author name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String toString(){
+
+        return title + " " + name + " " +price;
+    }
+}
